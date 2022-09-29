a6a852852eb1e2e7444cfa846b01909624b46d43
Peter Evers <pevers90@gmail.com>
2022-09-29 12:18:47 +0200

    chore: commit save point

--- src/example.py
+++ src/example.py
@@ -1,6 +1,9 @@
 import sys
 import pyfiglet
+from os.path import exists
 
 if __name__ == "__main__":
+  print(f"Input {sys.argv[[1]]}")
+  print(f"Exists? {exists(sys.argv[1])}")
   with open(sys.argv[1], "r") as f:
     print(pyfiglet.figlet_format(), end="")
